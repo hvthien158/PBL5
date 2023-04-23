62cd557e1ca448ac845eda78f46fec1e9df3c10f
Tra Van Thien <travanthien202@gmail.com>
2023-04-23 10:57:03 +0700

    test cors get all courses

--- src/main/java/com/pbl5/PBL5_Elearning/controller/CourseController.java
+++ src/main/java/com/pbl5/PBL5_Elearning/controller/CourseController.java
@@ -5,10 +5,7 @@ import com.pbl5.PBL5_Elearning.service.CoursesServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -20,6 +17,7 @@ public class CourseController {
     CoursesServiceImp coursesServiceImp;
 
     @GetMapping("/all")
+    @CrossOrigin
     public ResponseEntity<?> findAllCourse() {
         try {
             return new ResponseEntity<List<Courses>>(coursesServiceImp.find(), HttpStatus.OK);
