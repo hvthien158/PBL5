3765ade404b8db722c89a124c55d5459f042a6bc
Tra Van Thien <travanthien202@gmail.com>
2023-05-27 13:57:09 +0700

    change server v2

--- src/main/java/com/pbl5/PBL5_Elearning/repository/TeacherRepository.java
+++ src/main/java/com/pbl5/PBL5_Elearning/repository/TeacherRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TeacherRepository extends JpaRepository<Teacher, String> {
+public interface TeacherRepository extends JpaRepository<Teacher, Integer> {
 }
