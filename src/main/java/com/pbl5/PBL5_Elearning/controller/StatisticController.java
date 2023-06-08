0a961de9510e9867ba4cb323e88815ab09de5e1d
Tra Van Thien <travanthien202@gmail.com>
2023-06-08 15:15:09 +0700

    add statistic api v2

--- src/main/java/com/pbl5/PBL5_Elearning/controller/StatisticController.java
+++ src/main/java/com/pbl5/PBL5_Elearning/controller/StatisticController.java
@@ -7,6 +7,7 @@ import com.pbl5.PBL5_Elearning.payload.MessageResponse;
 import com.pbl5.PBL5_Elearning.service.CoursesServiceImp;
 import com.pbl5.PBL5_Elearning.service.UserCourseServiceImp;
 import com.pbl5.PBL5_Elearning.service.UserServiceImp;
+import io.swagger.models.auth.In;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -44,9 +45,49 @@ public class StatisticController {
                 return new ResponseEntity<MessageResponse>(new MessageResponse("Not admin"), HttpStatus.BAD_REQUEST);
             } else {
                 //Thống kê số lượng học viên mỗi khóa
-                return new ResponseEntity<List<Map<String, ?>>>(userCourseServiceImp.statistic(), HttpStatus.OK);
+                List<StatisticResponseFormat> listResponse = new ArrayList<StatisticResponseFormat>();
+                List<Map<String, ?>> list = userCourseServiceImp.statistic();
+                for(Map<String, ?> map : list){
+                    StatisticResponseFormat statisticResponseFormat = new StatisticResponseFormat();
+                    int id = Integer.valueOf(map.get("course_id").toString());
+                    statisticResponseFormat.setCourse_id(id);
+                    statisticResponseFormat.setCourse(coursesServiceImp.findById(id));
+                    statisticResponseFormat.setQuantity(Long.valueOf(map.get("quantity").toString()));
+                    listResponse.add(statisticResponseFormat);
+                }
+                return new ResponseEntity<List<StatisticResponseFormat>>(listResponse, HttpStatus.OK);
             }
         }
         return new ResponseEntity<MessageResponse>(new MessageResponse("Token invalid"), HttpStatus.BAD_REQUEST);
+    }
+
+    private static class StatisticResponseFormat{
+        private int course_id;
+        private long quantity;
+        private Courses course;
+
+        public int getCourse_id() {
+            return course_id;
+        }
+
+        public void setCourse_id(int course_id) {
+            this.course_id = course_id;
+        }
+
+        public long getQuantity() {
+            return quantity;
+        }
+
+        public void setQuantity(long quantity) {
+            this.quantity = quantity;
+        }
+
+        public Courses getCourse() {
+            return course;
+        }
+
+        public void setCourse(Courses course) {
+            this.course = course;
+        }
     }
 }
